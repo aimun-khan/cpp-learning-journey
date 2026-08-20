97c79713457361452350c650cf52b2ac745a853a
aimun-khan <khannawal2018@gmail.com>
2026-08-20 12:42:23 +0500

    Create if_else.cpp

---
+++ if_else.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int marks;
+cout << "Enter your marks: ";
+cin >> marks;
+if (marks >= 50) {
+cout << "Pass";
+} else { 
+cout << "Fail";
+}
+return 0;
+}
