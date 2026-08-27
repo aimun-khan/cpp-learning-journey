8f696fc609c4fd188f4530c2f51be9a11e2f4bdc
aimun-khan <khannawal2018@gmail.com>
2026-08-27 12:25:10 +0500

    Create boolean.cpp

---
+++ boolean.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    bool helpedFriend = false;
+
+    cout << "Did you help your friend? " << helpedFriend << endl;
+
+    helpedFriend = true;
+
+    cout << "After your decision: " << helpedFriend << endl;
+
+    return 0;
+}
